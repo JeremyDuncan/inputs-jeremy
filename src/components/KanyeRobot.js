14b116a184297e478faf0f2887005fd5d78930e9
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-02 19:18:38 -0400

    updated kanye bot text

--- src/components/KanyeRobot.js
+++ src/components/KanyeRobot.js
@@ -7,7 +7,7 @@ class KanyeRobot extends Component {
           <img className="robot-img" src={require("../images/kanya_bot.webp")} />
         </div>
         <div className="robot-title">
-          <h1>KANYEBOT 5000 ROBOT:</h1>
+          <h1>KANYEBOT 5000:</h1>
         </div>
         <div className="robot-speach">
           <h2>I'm gonna let you finish, but Beyonce is {this.props.userInput}.</h2>
